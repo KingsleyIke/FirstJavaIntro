f894692f39a1143538a601691df4b8de8333b91a
KingsleyOkoye <kingsdanike@gmail.com>
2020-11-30 09:49:00 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,20 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+    int number = 23;
+    double pointNum = 38.9977;
+    float floatNum = 46.77F;
+    String food = "Rice";
+    char character = 'A';
+System.out.println(NewClass.iHaveEaten(name));
+
+  }
+}
+
+
+  
+
+
+// static funtion that returns boolean called Ive eaten
+// takes in a string called food
+//if emty retur true else 
